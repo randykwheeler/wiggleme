d6425aa22bf5e46d40b953c97413b15a7b509157
Randy K. Wheeler <randy@wheeler.com>
2026-02-13 09:27:28 -0600

    Fix build: Removed incompatible icon resource and updated installer

--- build.bat
+++ build.bat
@@ -5,10 +5,10 @@ if not exist "%VCVARS%" set "VCVARS=C:\Program Files (x86)\Microsoft Visual Stud
 call "%VCVARS%"
 
 echo Compiling Resources...
-rc /fo resources.res resources.rc
+rc /fo resources.res resources.rc || echo Resource compilation skipped
 
 echo Building WiggleMe.exe...
-cl /EHsc /W4 /O2 main.cpp resources.res user32.lib gdi32.lib comctl32.lib gdiplus.lib shlwapi.lib /Fe:WiggleMe.exe
+cl /EHsc /W4 /O2 main.cpp resources.res user32.lib gdi32.lib comctl32.lib gdiplus.lib shlwapi.lib /Fe:WiggleMe.exe || cl /EHsc /W4 /O2 main.cpp user32.lib gdi32.lib comctl32.lib gdiplus.lib shlwapi.lib /Fe:WiggleMe.exe
 
 echo Packaging Assets...
 if exist WiggleMe.zip del WiggleMe.zip
